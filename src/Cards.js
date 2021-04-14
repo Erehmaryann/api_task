af2f7f63c247b26f3b114b6585fa6918519419eb
Maryann Edward <68599801+maryannedward@users.noreply.github.com>
2021-04-14 11:47:03 +0100

    refactored Cards Component

--- src/Cards.js
+++ src/Cards.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Card from './Card';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+import styles from './style/CardsStyle';
 
-const Cards = ({ movieData }) => {
+const Cards = (props) => {
+	const { movieData, classes } = props;
 	return (
-		<div>
-			<h1>This is the cards route</h1>
-			{movieData && movieData.map((movie) => <Card movie={movie} />)}
+		<div className={classes.Cards}>
+			<h1>List Of All Characters In Breaking Bad Series</h1>
+			<div className={classes.CardsCard}>{movieData && movieData.map((movie) => <Card movie={movie} />)}</div>
 			<Link to="/">Go to home</Link>
 		</div>
 	);
 };
 
-export default Cards;
+export default withStyles(styles)(Cards);
