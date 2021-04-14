30e3383f9221aac6fc800796d68f213cec9b43fa
Maryann Edward <68599801+maryannedward@users.noreply.github.com>
2021-04-14 11:47:32 +0100

    Refactored LandingPage

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Card from './Card';
+import { withStyles } from '@material-ui/styles';
 import { Link } from 'react-router-dom';
+import styles from './style/LandingPageStyle';
 
-const LandingPage = ({ movieData }) => {
+const LandingPage = (props) => {
+	const { movieData, classes } = props;
 	return (
-		<div>
-			<h1>This is the landing page</h1>
-			{movieData && movieData.slice(0, 6).map((movie) => <Card movie={movie} />)}
+		<div className={classes.LandingPage}>
+			<h1>List Of Some Characters In Breaking Bad Series</h1>
+			<div className={classes.LandingPageCards}>
+				{movieData && movieData.slice(0, 6).map((movie) => <Card movie={movie} />)}
+			</div>
 			<Link to="/cards">View all</Link>
 		</div>
 	);
 };
 
-export default LandingPage;
+export default withStyles(styles)(LandingPage);
